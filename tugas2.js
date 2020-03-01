38b8504a903f41a1555f75e8c40253f1653c6762
Mardionocs <mardionoputra19>
2020-03-02 06:01:34 +0700

    TUGAS 2 JS
    Tugas dari Niomic

---
+++ tugas2.js
@@ -0,0 +1,20 @@
+var number = 10;
+
+var string = "HALLO NIOMIC";
+
+var boolean = true;
+boolean = false;
+
+var nilai_null = "tidak kosong";
+nilai_null = null;
+
+var undifined;
+
+var array = [10,20,30,40,50,];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nilai_null);
+console.log(undifined);
+console.log(array);
